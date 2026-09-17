73db82f83620074c1f20d021662aed8836410594
Ashley <ash@sillyash.com>
2026-09-17 16:34:01 +0200

    Fix import paths in main.ts to use alias for App and router

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createI18n } from 'vue-i18n'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 
 const i18n = createI18n({
     locale: 'fr',
