d60568d7e08e93315cf0d73feb07966d26e7491a
Swann MERIENNE <swann.merienne@pc130-19.pedago.ensiie.fr>
2026-09-17 16:09:14 +0200

    Init

---
+++ web/src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+
+import App from './App.vue'
+import router from './router'
+
+const i18n = createI18n({
+    locale: 'fr',
+    fallbackLocale: 'en'
+})
+
+const app = createApp(App)
+
+app.use(i18n);
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
